e354db91053400dd204c290fee978ef4a83c6bf3
Archaons <louispakel@gmail.com>
2021-03-29 22:49:21 +0200

    First Commit

---
+++ Editor/Scripts/Core/ReverseMaskController2D.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ArchNet.Extension.ReverseMask2D
+{
+    /// <summary>
+    /// [EXTENSION] - [ARCH NET] - [REVERSE MASK 2D] Reverse Mask 2D Extension Controller
+    /// author : LOUIS PAKEL
+    /// </summary>
+    public class ReverseMaskController2D : MonoBehaviour
+    {
+        #region Serialized Fields 
+
+        [Header("MASKER")]
+        [SerializeField] private RectTransform _background;
+        [SerializeField] private CanvasGroup _masker;
+
+
+        #endregion
+
+        #region Private Fields
+
+        private List<Mask2D> _masks = new List<Mask2D>();
+
+        #endregion
+
+        #region Public  Methods
+
+
+        #region CRUD Mask
+        /// <summary>
+        /// Description : add mask 
+        /// </summary>
+        /// <param name="pMask"></param>
+        public void AddMask(Mask2D pMask)
+        {
+            if (_masks.Count == 0)
+            {
+                _masks.Add(pMask);
+            }
+            if (false == IsInList(pMask))
+            {
+                _masks.Add(pMask);
+            }
+        }
+
+        /// <summary>
+        /// Description : remove mask
+        /// </summary>
+        /// <param name="pMask"></param>
+        public void RemoveMask(Mask2D pMask)
+        {
+            if (true == IsInList(pMask))
+            {
+                _masks.Remove(pMask);
+            }
+        }
+
+        /// <summary>
+        /// Description : Check if a mask in already in the list
+        /// </summary>
+        /// <param name="pMask"></param>
+        /// <returns></returns>
+        public bool IsInList(Mask2D pMask)
+        {
+            bool lResult = false;
+
+            foreach (Mask2D lMask in _masks)
+            {
+                if (lMask.GetId() == pMask.GetId())
+                {
+                    lResult = true;
+                }
+            }
+
+            return lResult;
+        }
+
+        #endregion
+
+
+        #region Getter
+
+        /// <summary>
+        /// Description : return mask from the list
+        /// </summary>
+        /// <param name="pId"></param>
+        /// <returns></returns>
+        public Mask2D GetMask(int pId)
+        {
+            Mask2D lResult = null;
+
+            foreach (Mask2D lMask in _masks)
+            {
+                if (lMask.GetId() == pId)
+                {
+                    lResult = lMask;
+                }
+            }
+
+            return lResult;
+        }
+
+
+        /// <summary>
+        /// Description : Hide masker
+        /// </summary>
+        public void HideMasker()
+        {
+            GetMasker().alpha = 0f;
+            GetMasker().interactable = false;
+            GetMasker().blocksRaycasts = false;
+        }
+
+        /// <summary>
+        /// Description : Show masker
+        /// </summary>
+        public void ShowMasker()
+        {
+            GetMasker().alpha = 1f;
+            GetMasker().interactable = true;
+            GetMasker().blocksRaycasts = true;
+        }
+
+        /// <summary>
+        /// Description : retrun masker
+        /// </summary>
+        /// <returns></returns>
+        private CanvasGroup GetMasker()
+        {
+            return _masker;
+        }
+        #endregion
+
+
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+    }
+}
---
+++ Editor/Scripts/Model/Mask2D.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace ArchNet.Extension.ReverseMask2D
+{
+    /// <summary>
+    /// [EXTENSION] - [ARCH NET] - [REVERSE MASK 2D] Mask 2D Model for Reverse Mask 2D Extension
+    /// author : LOUIS PAKEL
+    /// </summary>
+    public class Mask2D : MonoBehaviour, IPointerClickHandler
+    {
+        #region Serialized Fields 
+
+        [SerializeField] private Canvas _canvas;
+
+        #endregion
+
+        #region Private Fields
+
+        private int _id;
+
+        #endregion
+
+        #region Getter
+
+        /// <summary>
+        /// Description : get canvas
+        /// </summary>
+        public Canvas GetCanvas()
+        {
+            return _canvas;
+        }
+
+        public int GetId()
+        {
+            return _id;
+        }
+
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Description : Set Canvas position in foreground
+        /// </summary>
+        public void ToForeground()
+        {
+            if (IsInit() == false)
+            {
+                InitMask();
+            }
+
+            CanvasExtension.SetCanvasToForeground(_canvas);
+        }
+
+        /// <summary>
+        /// Description : Reset Canvas position
+        /// </summary>
+        public void ResetCanvas()
+        {
+            if (IsInit() == false)
+            {
+                InitMask();
+            }
+
+            CanvasExtension.StopOverridingCanvas(_canvas);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Description : Init Mask
+        /// </summary>
+        public void InitMask()
+        {
+            if (GetComponent<Canvas>() == null)
+            {
+                gameObject.AddComponent<Canvas>();
+            }
+
+            if (GetComponent<GraphicRaycaster>() == null)
+            {
+                gameObject.AddComponent<GraphicRaycaster>();
+            }
+
+            if (_canvas == null)
+            {
+                _canvas = GetComponent<Canvas>();
+            }
+        }
+
+        /// <summary>
+        /// Description : Check if the mask is init
+        /// </summary>
+        /// <returns></returns>
+        private bool IsInit()
+        {
+            bool lResult = true;
+
+            if (GetComponent<Canvas>() == null)
+            {
+                lResult = false;
+            }
+
+            if (GetComponent<GraphicRaycaster>() == null)
+            {
+                lResult = false;
+            }
+
+            return lResult;
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            // DO SOMETHING
+        }
+
+        #endregion
+    }
+}
